7c90cd9dc4c036de1be255a990d1b91f0c0b3b7d
Hamad <HamadNabizada@gmail.com>
2022-11-09 16:34:19 -0800

    setting up express

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+import express from 'express'
+import dotenv from 'dotenv'
+
+
+dotenv.config()
+let port = process.env.PORT | 3000
+
+let app = express()
+
+app.listen(port, (error)=>{
+    if(!error){
+        console.log(`Server running on port: ${port}.`);
+    }
+    else{
+        console.log(`Error: ${error}`);
+    }
+})
