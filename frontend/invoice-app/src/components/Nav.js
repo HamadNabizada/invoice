e15bfce6f7ef7341c33e66e1b52e85e391d2a562
Hamad <HamadNabizada@gmail.com>
2022-11-10 19:53:33 -0800

    update navBar styling

--- frontend/invoice-app/src/components/Nav.js
+++ frontend/invoice-app/src/components/Nav.js
@@ -1,16 +1,27 @@
 import styles from '../styles/Nav.module.css'
 import logo from '../assets/logo.svg'
 import userIcon from '../assets/userIcon.svg'
+import light from '../assets/light.svg'
+import dark from '../assets/dark.svg'
+import {useState} from 'react'
+
 
 export default function Nav(){
+    
+    let [lightMode, setLightMode] = useState(true)
+    let theme = lightMode ? light : dark
+    let toggleTheme = ()=>{
+        setLightMode(prevMode =>!prevMode)
+    }
+
     return(
         <div className={styles.navContainer}>
             <div className={styles.logoContainer}>
-                <img className={styles.logo} src={logo} alt="logo image" />
+                <img className={styles.logo} src={logo} alt="logo" />
             </div>
             <div className={styles.middleNav}>
-                <div className={styles.theme}>
-                    Theme
+                <div className={styles.themeContainer}>
+                    <img onClick={toggleTheme} className={styles.theme} src={theme} alt='color theme toggle' />
                 </div>
             </div>
             <div className={styles.userIconContainer}>
