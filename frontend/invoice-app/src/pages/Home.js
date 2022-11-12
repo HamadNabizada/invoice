e689e484c7590c21ffacee992c422ca5e49de85b
Hamad <HamadNabizada@gmail.com>
2022-11-11 19:48:49 -0800

    added responsive design to home page

--- frontend/invoice-app/src/pages/Home.js
+++ frontend/invoice-app/src/pages/Home.js
@@ -20,7 +20,7 @@ export default function Home(){
     }
     let darkTheme = {
         textColor: '#ffffff',
-        minorTextColor: '##888ba5',
+        minorTextColor: 'lightgray',
         bgColor:'#141625',
         minorBGColor:'#1f203c',
         shadow: '1px 1px 5px #1f203c'
@@ -55,8 +55,18 @@ export default function Home(){
                         <h3 style={styleTheme.minorText} className={style.invoiceSubTitle}>There are 4 total invoices</h3>
                     </div>
                     <div className={style.newInvoiceAndFilterContainer}>
-                        <div>FILTER</div>
-                        <div>New Invoice</div>
+                        {/* <div className={style.filterContainer}>
+                            <label for='filterSelection' className={style.filterLabel}>Filter by: </label>
+                            <select name="filter" id="filterSelection">
+                                <option value="status">Status</option>
+                                <option value="status">Status</option>
+                                <option value="status">Status</option>
+                            </select>
+                        </div> */}
+                        <div className={style.newInvoice}>
+                            {/* <p className={style.newInvoicePlusBtn}>+</p> */}
+                            <p className={style.newInvoiceText}>New Invoice</p>
+                        </div>
                     </div>
                 </section>
                 <section className={style.invoices}>
@@ -65,7 +75,16 @@ export default function Home(){
                         <p style={styleTheme.minorText} className={style.invoiceDue}>Due 19 August 2021</p>
                         <p style={styleTheme.minorText} className={style.invoiceOwner}>Jensen Huang</p>
                         <p className={style.invoiceCost}>$1200.99</p>
-                        <p className={style.invoiceStatus}>PAID</p>
+                        <p className={style.invoiceStatus}>&#x2022; PAID</p>
+                        <p className={style.rightArrow}>&#8250;</p>
+                    </h2>
+                    <h2 style={styleTheme.subBG} className={style.invoice}>
+                        <p className={style.invoiceID}>#RT3080</p>
+                        <p style={styleTheme.minorText} className={style.invoiceDue}>Due 19 may 2021</p>
+                        <p style={styleTheme.minorText} className={style.invoiceOwner}>Jensff234234234234asdfen Huang</p>
+                        <p className={style.invoiceCost}>$1200.99</p>
+                        <p className={style.invoiceStatus}>&#x2022; PENDING</p>
+                        <p className={style.rightArrow}>&#8250;</p>
                     </h2>
                 </section>
             </main>
