caaaaa7ba1ce946e8eb7da41d71b25427d0c6288
Hamad <HamadNabizada@gmail.com>
2022-11-24 22:17:52 -0800

    trashBtn added to edit modal

--- frontend/invoice-app/src/components/EditInvoiceModal.js
+++ frontend/invoice-app/src/components/EditInvoiceModal.js
@@ -184,7 +184,26 @@ export default function EditInvoice(props){
         })
     }  
     let itemLineElementsArray = []
-    
+    function removeItemLine(e){
+        if(newJSON.itemList.length === 1){
+            alert('Cannot have 0 items in list!')
+            return
+        }
+        let listIndex =  parseInt(e.target.getAttribute('data-listIndex'))
+        let list = [...newJSON.itemList]
+        let newList =[]
+        for (let index = 0; index < list.length; index++) {
+            const item = list[index];
+            if(index !== listIndex) newList.push(item)    
+        }
+        setJSON( prevJSON =>{
+            return {
+                ...prevJSON,
+                itemList: newList
+            }
+        })
+        
+    }
     newJSON.itemList.forEach((listItem,i) => {
         let singleItemLineElement = (
             <div key={i} className={styles.singleItemLine}>
@@ -192,7 +211,7 @@ export default function EditInvoice(props){
                 <input required value={newJSON.itemList[i].itemQty}  data-listindex={i} id='itemQty' onChange={updateItemList} className={styles.itemTextStyle} style={bgColor} type="number" />
                 <input required value={newJSON.itemList[i].itemPrice} data-listindex={i} id='itemPrice' onChange={updateItemList} className={styles.itemTextStyle} style={bgColor} type="number" />
                 <p data-listindex={i} id='itemTextPrice' className={styles.itemTextPrice}>{newJSON.itemList[i].listItemTotalFormatted}</p>
-                {/* <div data-listindex={i} className={styles.trashIcon}><img src={trashIcon} alt="remove item" /></div> */}
+                <div onClick={removeItemLine} data-listindex={i} className={styles.trashIcon}><img className={styles.trashIcon} src={trashIcon} alt="remove item" /></div>
             </div>
         )
         itemLineElementsArray.push(singleItemLineElement)
